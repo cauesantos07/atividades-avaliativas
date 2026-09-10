0fe8fbb1433ac38429eb838ea2da517ac51d3534
cauesantos07 <cauesantosdev@gmail.com>
2026-09-10 11:20:07 -0300

    Create atividade.cpp

---
+++ atividade.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<stdbool.h>
+int main()
+
+    int a= 5;
+    int b= 10;
+    int Temp; 
+    
+    printf("Antes da troca: a= %d, b= %d" a, b);
+    
+    temp = a;
+    a = b;
+    b = temp;
+    printf("Depois da troca: a = %i,b=%i  " a,b);
