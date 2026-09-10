19431ccab5a365adbe3ff0043b817c0e6cb24a93
cauesantos07 <cauesantosdev@gmail.com>
2026-09-10 11:16:42 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include<stdbool.h>
+int main()
+{
+    char nome [100];
+    int idade;
+    float Altura;
+    bool CNH = false;
+    char resposta;
+
+    printf("Digite seu nome:");
+    scanf("%s", &nome);
+    printf("Nome: %s", nome);
+    
+    printf("\nDigite sua idade: ");
+    scanf("%i", &idade);
+    printf("Idade: %i", idade);
+    
+    
+    printf("\nDigite sua Altura: ");
+    scanf("%f", &Altura);
+    printf("Altura: %.2f", Altura);
+    
+    printf("\nPossui CNH?");
+    
+    scanf(" %s", &resposta);
+    
+     printf("Voce tem cnh ?");
+    scanf( " %s", &resposta);
+    if(resposta == 'S'|| resposta =='s'){
+        CNH = true;
+        printf(" TRUE");
+    }else if(resposta =='N'||resposta =='n'){
+        CNH = false;
+        printf("false");
+    }else{
+        printf("Entrada inválida");
+    }
+
+    
+    
+        
+    
+
+
+    
+
+
+}
