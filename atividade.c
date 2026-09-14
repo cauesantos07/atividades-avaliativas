4e1a9fbdffe6e2bd58f1312f762ebb01c8291fd7
cauesantos07 <cauesantosdev@gmail.com>
2026-09-14 10:35:21 -0300

    Create atividade.c

---
+++ atividade.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+    //Leia um número e exiba se ele é par ou ímpar
+
+
+
+int main()
+{
+
+int numero;
+
+    printf("digite o número:");
+    scanf("%d", &numero);
+    
+if (numero % 2 == 0)
+    {
+        printf("par");
+    }
+    
+else 
+    {
+        printf("ímpar");
+    }
+    
+    
+    
+}
