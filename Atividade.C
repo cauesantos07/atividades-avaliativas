6736b709544fe68d00dbacbc5a2e2b4bac7ff4af
cauesantos07 <cauesantosdev@gmail.com>
2026-09-14 11:04:21 -0300

    Create Atividade.C

---
+++ Atividade.C
@@ -0,0 +1,25 @@
+#include <stdio.h>
+   //Leia um número e exiba se ele é par ou ímpar
+int main()
+{
+
+int numero;
+
+float nota;
+
+    printf ("Digite a nota:");
+    scanf ("%f",&nota);
+
+if (nota >= 7)
+{
+    printf("Aprovado");
+}
+else if (nota > 5 && nota <= 6.9)
+{
+    printf("Recuperação");
+}
+else
+{
+    printf("Reprovado");
+}
+}
