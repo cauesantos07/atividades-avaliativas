ba98174d3881d1e9e665728f909c4dd5a3ad214c
cauesantos07 <cauesantosdev@gmail.com>
2026-09-16 16:02:23 -0300

    Update atividade.cpp

--- atividade.cpp
+++ atividade.cpp
@@ -1,3 +1,4 @@
+\\ (1.2) Crie duas variáveis A = 5 e B = 10 Faça a troca dos valores entre elas de modo que A passe a valer 10 e B passe a valer 5.
 #include <stdio.h>
 #include<stdbool.h>
 int main()
