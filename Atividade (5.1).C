14b297fa9da19c75d9f05dc0993567f8f34b28ea
cauesantos07 <cauesantosdev@gmail.com>
2026-09-16 18:08:18 -0300

    Create Atividade (5.1).C

---
+++ Atividade (5.1).C
@@ -0,0 +1,21 @@
+
+(5.1)
+// Solicite um número ao usuário e, utilizando o laço for,
+// imprima a tabuada completa desse número (de 1 a 10).
+
+#include <stdio.h>
+
+int main()
+{
+    int numero, i;
+
+    printf("Digite um numero: ");
+    scanf("%i", &numero);
+
+    for (i = 1; i <= 10; i++)
+    {
+        printf("%i x %i = %i\n", numero, i, numero * i);
+    }
+
+}
+
